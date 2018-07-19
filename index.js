e9d6a35f4703014d3125e4643c3f9eab7aaca4eb
Emma Fisher <emmarfisher99@gmail.com>
2018-07-19 12:24:12 -0600

    trying to finish

--- index.js
+++ index.js
@@ -119,22 +119,26 @@ function handleLogout(request, response) {
  ****************************************************************/
 
 function addToList(req, res) {
-    if (request.session.user != undefined) {
-        var name = req.body.name;
-        var price = req.body.price;
-        var email = request.session.user;
-        var url = req.body.url;
+    console.log("HEYYYY", req.session);
+    if (request.session != undefined) {
+        if (request.session.user != undefined) {
+            var name = req.body.name;
+            var price = req.body.price;
+            var email = request.session.user;
+            var url = req.body.url;
 
-        pool.query(`INSERT INTO items(name, price, user_id, url) VALUES('${name}', '${price}', '${email}', '${url}')`, function (err, result) {
-            if (err) {
-                if (err.code === 'ETIMEDOUT') {
-                    console.log("timeout error");
+            pool.query(`INSERT INTO items(name, price, user_id, url) VALUES('${name}', '${price}', '${email}', '${url}')`, function (err, result) {
+                if (err) {
+                    if (err.code === 'ETIMEDOUT') {
+                        console.log("timeout error");
+                    }
+                    throw err;
                 }
-                throw err;
-            }
 
-            res.json(result);
-        })
+                res.json(result);
+            })
+        }
+
     } else {
         res.writeHead(301, {
             Location: "/public/login.html"
